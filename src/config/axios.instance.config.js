df7375de9d6458f2277b6ff430acfa835b3d1687
Rasulwebs <boboqulovrasul80@gmail.com>
2023-09-19 16:00:35 +0500

    edited links

--- src/config/axios.instance.config.js
+++ src/config/axios.instance.config.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const token = window.localStorage.getItem("token");
 
 export const instance = axios.create({
-  // baseURL: "https://fatkhullo.backend4devs.uz", 
-  baseURL: "http://64.226.90.160:3005",
+  baseURL: "https://fatkhullo.backend4devs.uz", 
+  // baseURL: "http://64.226.90.160:3005",
   headers: {
     "Content-Type": "application/json",
     token: `${token}`,
