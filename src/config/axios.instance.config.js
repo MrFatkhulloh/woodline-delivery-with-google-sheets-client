62b64cd99474c0290262b18debdf1d6d9cc02326
MrFatkhulloh <91322226+MrFatkhulloh@users.noreply.github.com>
2023-09-25 18:42:52 +0500

    Update axios.instance.config.js

--- src/config/axios.instance.config.js
+++ src/config/axios.instance.config.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const token = window.localStorage.getItem("token");
 
 export const instance = axios.create({
-  // baseURL: "https://fatkhullo.backend4devs.uz",
-  baseURL: "https://mirabdullo.backend4devs.uz",
+  baseURL: "https://fatkhullo.backend4devs.uz",
+  // baseURL: "https://mirabdullo.backend4devs.uz",
   headers: {
     "Content-Type": "application/json",
     token: `${token}`,
